ec8b3f18d20ab28665d2c0dcb3215e7585edee5c
UtsavUpadhyay08 <utsavup2004@gmail.com>
2024-07-16 20:38:56 +0530

    dsu

--- graph_algo_1.cpp
+++ graph_algo_1.cpp
@@ -40,118 +40,37 @@ ll lcm(ll a, ll b){return (a/gcd(a,b))*b;}
 class Graph{
 	ll n;
 	vector<vpll> adj;
-	// vector<bool> vis;
-	vll vis;
-	vll dis,topo;
-	vll indegree;
+	vll link,size;
 	public:
 	Graph(ll n){
 		this->n=n;
 		vector<vpll> adj(n+1);
-		// vector<bool> vis(n+1,false);
-		vll vis(n+1,0);
-		vll dis(n+1,-1);
 		this->adj=adj;
-		this->vis=vis;
-		this->dis=dis;
-		vll indegree(n+1,0);
-		this->indegree=indegree;
+		vll link(n+1),size(n+1,1);
+		fori(i,0,n+1) link[i]=i;
+		this->link=link;
+		this->size=size;
 	}
 	void addedge(ll a,ll b,ll wt=0ll){
 		adj[a].pb({b,wt});
 		// adj[b].pb({a,wt});			//if directed graph comment this
-		indegree[b]++;
 	}
-	void dfs(ll i){
-		if(vis[i]) return;
-		vis[i]=true;
-		for(auto it:adj[i]){
-			if(!vis[it.f]) dfs(it.f);
-		}
+	ll find(ll a){
+		if(link[a]==a) return a;		// finds the parent
+		return find(link[a]);
 	}
-	void bfs(ll strt){
-		queue<ll> q;
-		q.push(strt);
-		while(!q.empty()){
-			ll vl=q.front();
-			q.pop();
-			if(vis[vl]) continue;
-			vis[vl]=true;
-			for(auto it:adj[vl]){
-				if(!vis[it.f]) q.push(it.f);
-			}
-		}
+	bool same(ll a,ll b){				//Check for same parent
+		return find(a)==find(b);
 	}
-	vll djikstra(ll node){
-		priority_queue<pll> pq;
-		pq.push({0,node});
-		dis[node]=0;
-		while(!pq.empty()){
-			pll vl=pq.top();
-			pq.pop();
-			if(vis[vl.sc]) continue;
-			vis[vl.sc]=true;
-			vl.f*=(-1);
-			for(auto it:adj[vl.sc]){
-				ll node2=it.f,wt=it.sc;
-				if((dis[node2]==-1) || dis[node2]>(vl.f+wt)){
-					dis[node2]=(vl.f+wt);
-					pq.push({-1*dis[node2],node2});
-				}
-			}
-		}
-		return dis;
-	}
-	bool isCycleDAG(ll i){			//Directed Graph
-		if(vis[i]) return true;
-		vis[i]=true;
-		bool ans=false;
-		for(auto it:adj[i]) ans=ans|isCycleDAG(it);
-		return ans;
-	}
-	bool isCycle(ll node,ll parent){		//Undirected graph
-		if(vis[node]==2) return false;
-		if(vis[node]==1) return true;
-		vis[node]=1;
-		bool ans=false;
-		for(auto it:adj[node]){
-			if(it.f!=parent) ans=ans|isCycle(it.f,node);
-		}
-		vis[node]=2;
-		return ans;
-	}
-	void topodfs(ll i){
-		if(vis[i]==2) return;
-		// if(vis[i]==1)   		//Cycle exists
-		vis[i]=1;
-		for(auto it:adj[i]){
-			topodfs(it.f);
-		}
-		vis[i]=2;
-		topo.pb(i);
-	}
-	vll toposort(){
-		for(ll node=1;node<=n;node++){		//Change according to numbering of nodes
-			topodfs(node);
-		}
-		reverse(all(topo));
-		return topo;
-	}
-	vll kahn(){
-		queue<ll> q;
-		fori(i,1,n+1) if(indegree[i]==0) q.push(i);
-		vll ans;
-		while(!q.empty()){
-			ll vl=q.front();
-			q.pop();
-			ans.pb(vl);
-			for(auto it:adj[vl]){
-				indegree[it.f]--;
-				if(indegree[it.f]==0) q.push(it.f);
-			}
-		}
-		if((ll)ans.size()>n) return {};
-		return ans;
+	void unite(ll a,ll b){
+		a=find(a);
+		b=find(b);
+		if(size[a]<size[b]) swap(a,b);
+		size[a]+=size[b];
+		link[b]=a;
+		debug(a,b)
+		debug(link)
+		debug(size)
 	}
 };
 
@@ -162,12 +81,9 @@ void solve(){
 	fori(i,0,m){
 		ll a,b;cin>>a>>b;
 		g.addedge(a,b);
+		g.unite(a,b);
 	}
-	// debug(g.toposort());
-	// debug(g.kahn());
-	// debug(g.isCycle(1,-1));
-	// g.dfs(1);
-	// debug(g.djikstra(1));
+	debug(g.same(3,5))
 	// vll a(n);for(auto &x:a) cin>>x;
 	// string s;cin>>s;
 }
