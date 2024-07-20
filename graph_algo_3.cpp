d15919d352bf6c864e0683b1c040b436adcab989
UtsavUpadhyay08 <utsavup2004@gmail.com>
2024-07-20 19:24:02 +0530

    KOsaraju ALgo

--- graph_algo_3.cpp
+++ graph_algo_3.cpp
@@ -38,10 +38,11 @@ ll lcm(ll a, ll b){return (a/gcd(a,b))*b;}
 
 
 class Graph{
+	public:
 	ll n;
 	vector<vpll> adj;
 	vector<bool> vis;
-	public:
+	vpll edges;
 	Graph(ll n){
 		this->n=n;
 		adj.resize(n+1);
@@ -49,17 +50,61 @@ class Graph{
 	}
 	void addedge(ll a,ll b,ll wt=0ll){ 		//if undirected graph call this for a,b then b,a
 		adj[a].pb({b,wt});
+		edges.pb({a,b});
 	}
 };
 
+class Kosaraju:public Graph{
+	stack<ll> st;
+	public:
+	Kosaraju(ll n):Graph(n){}
+	vector<vll> algo(){
+		vector<vll> ans;
+		fori(i,1,n+1) dfs1(i);
+		reverse();
+		while(!st.empty()){
+			ll vl=st.top();
+			st.pop();
+			if(vis[vl]) continue;
+			vll scc;
+			dfs2(vl,scc);
+			ans.pb(scc);
+		}
+		return ans;
+	}
+	void reverse(){
+		vis.clear();
+		vis.resize(n+1,false);
+		adj.clear();
+		adj.resize(n+1);
+		for(auto it:edges) addedge(it.sc,it.f);
+	}
+	void dfs1(ll node){
+		if(vis[node]) return;
+		vis[node]=true;
+		for(auto it:adj[node]){
+			if(!vis[it.f]) dfs1(it.f);
+		}
+		st.push(node);
+	}
+	void dfs2(ll node,vll &scc){
+		if(vis[node]) return;
+		vis[node]=true;
+		scc.pb(node);
+		for(auto it:adj[node]) if(!vis[it.f]) dfs2(it.f,scc);
+	}
+};
 
 void solve(){
 	ll n,m;cin>>n>>m;
-	Graph g(n);
+	Kosaraju g(n);
 	fori(i,0,m){
-		ll a,b,wt;cin>>a>>b>>wt;
-		g.addedge(a,b,wt);
-		g.addedge(b,a,wt);
+		ll a,b;cin>>a>>b;
+		g.addedge(a,b);
+	}
+	vector<vll> ans=g.algo();
+	fori(i,0,ans.size()){
+		debug(i);debug(ans[i]);
 	}
 	// vll a(n);for(auto &x:a) cin>>x;
 	// string s;cin>>s;
@@ -70,7 +115,7 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     
-	// solve();
-    ll t;cin>>t;while(t--){solve();cout<<"\n";}
+	solve();
+    // ll t;cin>>t;while(t--){solve();cout<<"\n";}
     return 0;
 }
